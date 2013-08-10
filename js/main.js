906b58a45ef87fb1ebf660b734b8d21cc1fbfb67
Jessica Bertling <berlting.jess@gmail.com>
2013-08-09 21:29:36 -0400

    New page search refinement functionality

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	getResultCount();
+	sortResults();
+	
+	function getResultCount() {
+		var countResults = 0;
+		$(".searchResults article").not(":hidden").each(function() {
+			countResults++;
+		});
+		if (countResults > 0) {
+			$(".searchResults .noResultsFound").hide();
+			$(".resultsCount span").html(countResults);
+		}
+		else {
+			$(".searchResults .noResultsFound").show();
+			$(".searchResults article").slideDown();
+			$(".resultsCount span").html(countResults);
+		}
+		
+	}
+	
+	function sortResults() {
+		$(".refineSearch .refinement").click(function() {
+			$(this).siblings().removeClass("active");
+			$(this).addClass("active");
+			var dataSet = $(this).parent().attr("data-set");
+			var getDataSet = "data-" + dataSet;
+			if (dataSet == "city") {
+				var sortTerm = $(this).attr(getDataSet);
+				$(".searchResults article").each(function() {
+					var sortOfData = $(this).attr(getDataSet);
+					if (sortOfData != sortTerm) {
+						$(this).slideUp("fast", function() {
+							getResultCount();
+						});
+					}
+					else {
+						$(this).slideDown("fast", function() {
+							getResultCount();
+						});
+					}
+				})
+			}
+			else if (dataSet == "amenities") {
+				alert('amenities');
+			}
+		});
+	}
+});
